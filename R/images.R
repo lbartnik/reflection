d80d4c7988d2220a9925053dac46b64ed7da8b33
Lukasz A. Bartnik <l.bartnik@gmail.com>
2018-10-09 23:47:47 -0700

    docs

--- R/images.R
+++ R/images.R
@@ -1,3 +1,21 @@
+#' Unwrap an image.
+#'
+#' Unwrapping means transforming image from polar coordinates into
+#' carthsian coordinates. First, the center of the polar coordinate
+#' system is placed at the center of the image rectangle. Then, pixel
+#' values are read for each angle `alpha` from `0` to `2*Pi`, with a
+#' step of `dAlpha`, and distance `r` from `0` to `diag/2`, with a step
+#' of `dR`. Each sun "ray" becomes a column of the output image.
+#'
+#' Because maximum distance `diag/2` defines a circle extending beyond
+#' the input image `x`, pixel values outside of `x` are assumed to be
+#' equal to `missing`.
+#'
+#' @param x Input image.
+#' @param dAlpha Step for the angle `alpha`.
+#' @param dR Step for the distance `r`.
+#' @param missing The value for pixels outside of `x`.
+#'
 #' @importFrom imager is.cimg grayscale squeeze
 #' @export
 unwrap_image <- function (x, dAlpha = 1, dR = 1, missing = 1) {
@@ -16,7 +34,45 @@ unwrap_array <- function (x, dAlpha = 1, dR = 1, rMax = NULL, missing = 0) {
 }
 
 
+#' Compute distance between two images.
+#'
+#' Important: images are compared column-wise, thus they must have equal
+#' numbers of columns. Distance is computed in two phases: preparation
+#' followed by computing the proper distance.
+#'
+#' During the preparation, each image is first turned into scale of
+#' grays with [imager::grayscale]. Then, gradient of color changes is
+#' computed for each image as `imgradient(i, 'xy')` (see [images::imgradient]).
+#' Only gradients larger than `quantile(gradient, cutoff)` are kept.
+#'
+#' In the second phase, images are matched column-wise, creating pairs
+#' of columns. For each column the distances from the bottom of the image
+#' (bottom row) to each non-zero gradient value is calculated. These
+#' values are then treated as results of sampling a random variable.
+#' The empirical cumulative distribution function (_ecdf_) is obtained
+#' for each column and the area between matching _ecdf_s is calculated.
+#' This is similar to how the Kolmogorov-Smirnov test is performed.
+#'
+#' Finally, the total area for all pairs of columns is returned as the
+#' distance between images `a` and `b`.
+#'
+#' For optimal results images should be always _unwrapped_; see
+#' [unwrap_image].
+#'
+#' @param a First image; see [imager::cimg].
+#' @param b Second image.
+#' @param cutoff The cut-off quantile for gradient values.
+#' @return Cumulative area between _ecdf_s for all pairs of columns.
+#'
+#' @importFrom imager is.cimg grayscale squeeze enorm imgradient
 #' @export
+#'
+#' @examples
+#' \dontrun{
+#'   exp <- load.image('roc.png')
+#'   img <- load.image('test.png')
+#'   image_dist(unwrap_image(test), unwrap_image(exp))
+#' }
 image_dist <- function (a, b, cutoff = .5) {
   stopifnot(is.cimg(a), is.cimg(b))
 
@@ -26,7 +82,7 @@ image_dist <- function (a, b, cutoff = .5) {
   }
 
   to_distances <- function (x) {
-    x <- imgradient(x, "xy") %>% enorm %>% as_grayscale %>% as.array
+    x <- as_grayscale %>% imgradient(x, "xy") %>% enorm %>% as.array
     apply(x > quantile(as.numeric(x), cutoff), 1, function (c) which(c)/length(c))
   }
 
